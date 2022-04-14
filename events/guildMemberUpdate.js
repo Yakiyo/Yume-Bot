e09e2ccc99a3be3ec7b279d324f40a2d3bc2f579
Yakiyo <yakiyosama5855@gmail.com>
2022-04-14 15:09:06 +0600

    add boostwithdrawal message

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -14,6 +14,10 @@ module.exports = {
         if (boostRole !== undefined && boostRole !== null) {
             if (!oldMember.roles.cache.has(`${boostRole.id}`) && newMember.roles.cache.has(`${boostRole.id}`)) {
                 await booster(newMember, guild, client);
+            } else if (oldMember.roles.cache.has(`${boostRole.id}`) && !newMember.roles.cache.has(`${boostRole.id}`)) {
+                const staffChan = await client.channels.fetch('844632108831342679').then(chan => chan);
+
+                await staffChan.send(`${newMember.user.tag} withdrew server boosts!`);
             }
         }
         return;
