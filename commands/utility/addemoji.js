7bea847e3a1b77fc7cf64a257ba56e9779fd62d3
Yakiyo <yakiyosama5855@gmail.com>
2022-04-29 21:09:37 +0600

    create addemoji command

---
+++ commands/utility/addemoji.js
@@ -0,0 +1,24 @@
+module.exports = {
+    name: 'addemoji',
+    description: 'adds an emoji to the server',
+    aliases: ['steal'],
+    usage: '<emoji name> <image link>',
+    perms: 'MANAGE_EMOJIS_AND_STICKERS',
+    category: 'utility',
+    args: true,
+    async execute(message, args) {
+        console.log(args);
+        const name = args[0];
+        const url = args[1];
+        if (!/^https?:\/\/.*\/.*\.(png|gif|webp|jpeg|jpg)\??.*$/gmi.test(url)) return message.channel.send('Second argument does not seem to match a valid image link expression!');
+
+        if (message.guild.me.permissions.has('MANAGE_EMOJIS_AND_STICKERS') == false) return message.channel.send('I do not have the required permissions to add emojis in this server.');
+        return await message.guild.emojis.create(url, name)
+                .then(emoji => {
+                    message.channel.send(`Successfully created emoji with name **${emoji.name}**`);
+                })
+                .catch(() => {
+                     message.channel.send('Error while creating emoji. \nPossible Reasons: Image file too big, invalid file type, maximum emoji limit for the server.');
+                });
+    },
+};
