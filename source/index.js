57b5b5d87d666b6d57eed82febbe59ac3bc9eb60
Yakiyo <yakiyosama5855@gmail.com>
2022-05-19 18:48:01 +0600

    Setup index.js
    * add event handler
    * add interaction handler
    * add loading slash commands on login

--- source/index.js
+++ source/index.js
@@ -1,5 +1,9 @@
-const fs = require('fs');
+const fs = require('node:fs');
 const { Client, Intents, Collection } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId } = require('./config.json');
+const path = require('node:path');
 require('dotenv').config();
 
 const client = new Client({
@@ -28,18 +32,50 @@ const client = new Client({
 		repliedUser: true,
 	},
 });
+const botId = process.env.NODE_ENV === 'production' ? clientId : '964798451261014026';
 
-const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+const commands = [];
+client.commands = new Collection();
+const commandFolders = fs.readdirSync('./source/commands');
+
+for (const folder of commandFolders) {
+	const commandFiles = fs.readdirSync(`./source/commands/${folder}`).filter(file => file.endsWith('.js'));
+
+	for (const file of commandFiles) {
+		const command = require(path.resolve(process.cwd(), `./source/commands/${folder}/${file}`));
+		command.category = `${folder}`;
+		client.commands.set(command.data.name, command);
+		commands.push(command.data.toJSON());
+	}
+}
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationGuildCommands(botId, guildId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+const eventFiles = fs.readdirSync('./source/events').filter(file => file.endsWith('.js'));
 
 for (const file of eventFiles) {
-	const event = require(`./events/${file}`);
+	const event = require(path.resolve(process.cwd(), `./source/events/${file}`));
 	if (event.once) {
 		client.once(event.name, (...args) => event.execute(...args, client));
 	} else {
 		client.on(event.name, (...args) => event.execute(...args, client));
 	}
 }
-
 
 // Login to Discord with your client's token
 client.login(process.env.TOKEN);
