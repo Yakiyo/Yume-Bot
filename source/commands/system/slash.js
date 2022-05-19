2033a2c0ef5441829b09a5394d29302dfa03ff71
Yakiyo <yakiyosama5855@gmail.com>
2022-05-19 19:44:11 +0600

    Create command to dynamically deploy commands

---
+++ source/commands/system/slash.js
@@ -0,0 +1,61 @@
+const fs = require('node:fs');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, owners } = require('../../config.json');
+const { Collection } = require('discord.js');
+const path = require('path');
+require('dotenv').config();
+const botId = process.env.NODE_ENV === 'production' ? clientId : '964798451261014026';
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('slash')
+        .setDescription('Deploys slash command to local guild')
+        .addBooleanOption(option =>
+            option.setName('clear')
+                .setDescription('Wether to clear commands or not. true for clearing, false for not. Defaults to false')),
+    async execute(interaction) {
+        if (!owners.includes(interaction.user.id)) return await interaction.reply('This command is only usable by the bot dev');
+        await interaction.deferReply();
+        const option = interaction.options.getBoolean('clear') || false;
+        const commands = [];
+
+        if (!option) {
+            const clientCommands = new Collection();
+            const commandFolders = fs.readdirSync('./source/commands');
+
+            for (const folder of commandFolders) {
+                const commandFiles = fs.readdirSync(`./source/commands/${folder}`).filter(file => file.endsWith('.js'));
+
+                for (const file of commandFiles) {
+                    const command = require(path.resolve(process.cwd(), `./source/commands/${folder}/${file}`));
+                    command.category = `${folder}`;
+                    clientCommands.set(command.data.name, command);
+                    commands.push(command.data.toJSON());
+                }
+            }
+            interaction.client.commands = clientCommands;
+        } else {
+            const slash = interaction.client.commands.get(interaction.commandName);
+            commands.push(slash.data.toJSON());
+        }
+
+        (async () => {
+            try {
+                await interaction.editReply('Started refreshing application (/) commands....');
+
+                await rest.put(
+                    Routes.applicationGuildCommands(botId, guildId),
+                    { body: commands },
+                );
+
+                return await interaction.editReply('Successfully registered guild commands!');
+            } catch (error) {
+                console.error(error);
+                return await interaction.editReply('Failed to register guild commands. :x:');
+            }
+        })();
+    },
+};
