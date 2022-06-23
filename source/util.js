1e861acf943e260c27cc09ce7427e900c265207c
Yakiyo <yakiyosama5855@gmail.com>
2022-06-23 13:55:07 +0600

    update utils
    * change object to class and export an instance of it
    
    * add jsdoc comments to functions

--- source/util.js
+++ source/util.js
@@ -3,22 +3,36 @@ const turndownService = require('turndown');
 const turndown = new turndownService;
 // converts the span element spoilers (for anilist) to discord spoiler markdown
 turndown.addRule('spoiler', {
-	filter: ['span'],
-	replacement: function(text) {
-		return `|| ${text} ||`;
-	},
-});
-const util = {
-    shorten(string, num = 1000) {
-            if (typeof string !== 'string') return undefined;
-
-            const str = turndown.turndown(string);
-            if (str.length > num) {
-                return str.substring(0, num + 1) + '...';
-            } else {
-                return str;
-            }
+    filter: ['span'],
+    replacement: function (text) {
+        return `|| ${text} ||`;
     },
+});
+/**
+ * Utility class with some functions for making life easier
+ */
+class Util {
+    /**
+     * Shortens a string upto provided length
+     * @param {string} string the string to shorten
+     * @param {number} num length upto which to shorten
+     * @returns string
+     */
+    shorten(string, num = 1000) {
+        if (typeof string !== 'string') return undefined;
+
+        const str = turndown.turndown(string);
+        if (str.length > num) {
+            return str.substring(0, num + 1) + '...';
+        } else {
+            return str;
+        }
+    }
+    /**
+     * Coverts the first character of the string to uppercase and the rest to lowercase
+     * @param {string} string the string to casify
+     * @returns string
+     */
     casify(string) {
         if (typeof string == 'string') {
             const space = string.split(/_/g);
@@ -26,7 +40,13 @@ const util = {
         } else {
             return 'null';
         }
-    },
+    }
+    /**
+     * Sends an embed to the modlogs channel
+     * @param {Client} client
+     * @param {MessageEmbed} embed
+     * @returns Message
+     */
     async modlog(client, embed) {
         const channel = await client.channels.fetch(modlogs).catch(() => null);
         if (!channel) {
@@ -34,7 +54,7 @@ const util = {
             return null;
         }
         return await channel.send({ embeds: [embed] }).catch((error) => console.log(error));
-    },
-};
+    }
+}
 
-module.exports = util;
+module.exports = new Util();
