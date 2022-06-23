64e4fe9d891131d5fae37000ac09a33ada7f531b
Yakiyo <yakiyosama5855@gmail.com>
2022-06-23 12:14:05 +0600

    finish up ban command
    * add unban command
    * add hackban command

--- source/commands/moderation/ban.js
+++ source/commands/moderation/ban.js
@@ -2,9 +2,9 @@ const { SlashCommandBuilder } = require('@discordjs/builders');
 const { modlog } = require('../../util.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ban')
-		.setDescription('Ban related commands')
+    data: new SlashCommandBuilder()
+        .setName('ban')
+        .setDescription('Ban related commands')
         .addSubcommand(sub =>
             sub.setName('add')
                 .setDescription('Bans a user')
@@ -24,9 +24,19 @@ module.exports = {
                         .setRequired(true))
                 .addStringOption(option =>
                     option.setName('reason')
-                        .setDescription('Reason for unbanning the user'))),
-	async execute(interaction) {
-		await interaction.deferReply();
+                        .setDescription('Reason for unbanning the user')))
+        .addSubcommand(sub =>
+            sub.setName('hack')
+                .setDescription('Bans a user using an id. Useful if user is not in server')
+                .addStringOption(option =>
+                    option.setName('user')
+                        .setDescription('Id of the user to ban.')
+                        .setRequired(true))
+                .addStringOption(option =>
+                    option.setName('reason')
+                        .setDescription('Reason for banning the user'))),
+    async execute(interaction) {
+        await interaction.deferReply();
         if (!interaction.guild.me.permissions.has('BAN_MEMBERS')) return await interaction.editReply('I do not have the required permissions to ban or unban users.');
         const subCommand = interaction.options.getSubcommand();
 
@@ -52,7 +62,7 @@ module.exports = {
                 });
                 await modlog(interaction.client, {
                     title: 'Ban Case',
-                    color: 'GOLD',
+                    color: 14507859,
                     timestamp: new Date(),
                     footer: {
                         text: `ID: ${user.user.id}`,
@@ -64,6 +74,45 @@ module.exports = {
                 console.log(error);
                 return await interaction.editReply('Unexpected internal error. Could not successfully ban user. Please check console for error information');
             }
+        } else if (subCommand === 'remove') {
+            const user = await interaction.guild.bans.fetch(interaction.options.getString('user')).then(ban => ban?.user).catch(() => null);
+            if (!user) return await interaction.editReply('No banned user with that id found. Make sure you\'re using a valid user id who has been previously banned');
+            try {
+                await interaction.guild.members.unban(user, interaction.options.getString('reason') || 'No Reason Provided');
+                await modlog(interaction.client, {
+                    title: 'Unban Case',
+                    color: 5496228,
+                    timestamp: new Date(),
+                    footer: {
+                        text: `ID: ${user.id}`,
+                    },
+                    description: `**Offender:** ${user.tag} | <@!${user.id}>\n**Moderator:** ${interaction.user.tag}\n**Reason:** ${interaction.options.getString('reason') || 'No reason provided'}`,
+                });
+                return await interaction.editReply(`Successfully unbanned ${user.tag}`);
+            } catch (error) {
+                console.log(error);
+                return await interaction.editReply('Unexpected internal error. Could not unban user successfully. Please check console for error');
+            }
+        } else if (subCommand === 'hack') {
+            const id = interaction.options.getString('user');
+            try {
+                await interaction.guild.bans.create(id, {
+                    reason: interaction.options.getString('reason') || 'No Reason Provided',
+                });
+            } catch (error) {
+                console.log(error);
+                return await interaction.editReply('Error when attempting to ban. Please make sure provided argument is a valid user id');
+            }
+            await modlog(interaction.client, {
+                title: 'Ban Case - (H)',
+                color: 14507859,
+                timestamp: new Date(),
+                footer: {
+                    text: `ID: ${id}`,
+                },
+                description: `**Offender:** <@!${id}>\n**Moderator:** ${interaction.user.tag}\n**Reason:** ${interaction.options.getString('reason') || 'No reason provided'}`,
+            });
+            return await interaction.editReply(`Successfully banned user with id ${id}`);
         }
     },
 };
