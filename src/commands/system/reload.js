e81d97f69659fe37a3a5eabd81948c3887b42a37
Yakiyo <yakiyosama5855@gmail.com>
2022-06-28 16:41:51 +0000

    Create reload command for interactions

--- src/commands/system/reload.js
+++ src/commands/system/reload.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { owners } = require('../../config.json');
+const fs = require('node:fs');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -10,11 +11,29 @@ module.exports = {
                 .setDescription('The command to reload')
                 .setRequired(true)),
     async execute(interaction) {
-        await interaction.deferReply();
-
 		if (!owners.includes(interaction.user.id)) {
-            return await interaction.editReply({ content: 'This command can only be executed by bot dev.', ephemeral: true });
+            return await interaction.reply({ content: 'This command can only be executed by bot dev.', ephemeral: true });
         }
-        await interaction.editReply('done');
+        await interaction.deferReply({ ephemeral: true });
+        const command = interaction.client.commands.get(interaction.options.getString('command'));
+        console.log(command);
+        if (!command) return await interaction.editReply('Could not find any command with that name.');
+        let folder;
+        if (command.category) {
+            folder = command.category;
+        } else {
+            const commandFolders = fs.readdirSync('./src/commands');
+            folder = commandFolders.find(subfolder => fs.readdirSync(`./src/commands/${subfolder}`).includes(`${command.data.name}.js`));
+
+        }
+
+        delete require.cache[require.resolve(`../${folder}/${command.data.name}.js`)];
+        try {
+            const reloaded = require(`../${folder}/${command.data.name}.js`);
+            interaction.client.commands.set(reloaded.data.name, reloaded);
+            return await interaction.editReply(`Command ${reloaded.data.name} was successfully reloaded`);
+        } catch (error) {
+            return await interaction.editReply('Something went wrong');
+        }
     },
 };
