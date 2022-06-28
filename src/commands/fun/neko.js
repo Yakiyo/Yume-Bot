bb3e734e46ae839ae40bf0adb5d93d965f213d86
Yakiyo <yakiyosama5855@gmail.com>
2022-06-28 17:36:48 +0000

    Create neko command

---
+++ src/commands/fun/neko.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { nekos } = require('../../util.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('neko')
+        .setDescription('Gives a neko art'),
+    async execute(interaction) {
+        await interaction.deferReply();
+        const result = await nekos('neko');
+        if (!result) return await interaction.editReply('Internal error when attempting to fetch image from API. It might be down at this moment. Please try again later');
+        const embed = {
+            title: 'Here\'s a neko for you.',
+            color: 15277667,
+            description: `Image Source: [link](${result.source_url || 'https://nekos.best'} 'Source') || Artist: [${result.artist_name || 'Unknown'}](${result.artist_href || 'https://nekos.best'} 'Artist')`,
+            image: {
+                url: `${result.url}`,
+            },
+            footer: {
+                text: 'Powered by nekos.best',
+                icon_url: 'https://i.imgur.com/IMhljcr.png',
+            },
+        };
+        return await interaction.editReply({ embeds: [embed] });
+    },
+};
