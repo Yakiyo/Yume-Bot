a6924e4af691542105761361c3648297e824ec51
Yakiyo <yakiyosama5855@gmail.com>
2022-08-05 11:42:32 +0000

    feat: create channel create subcommand

--- src/commands/moderation/channel.js
+++ src/commands/moderation/channel.js
@@ -8,6 +8,7 @@ const { casify, shorten } = require('../../util.js');
         create
         delete
         rename
+        clone
 */
 
 module.exports = {
@@ -20,7 +21,25 @@ module.exports = {
                 .addChannelOption(option =>
                     option.setName('channel')
                         .setDescription('The channel to return info of.')
-                        .addChannelTypes(CT.GuildText, CT.GuildVoice, CT.GuildNews))),
+                        .addChannelTypes(CT.GuildText, CT.GuildVoice, CT.GuildNews)))
+        .addSubcommand(sub =>
+            sub.setName('create')
+                .setDescription('Create a new channel')
+                .addStringOption(option =>
+                    option.setName('name')
+                        .setDescription('Name of the channel to be created')
+                        .setRequired(true))
+                .addStringOption(option =>
+                    option.setName('type')
+                        .setDescription('The type of the channel. Defaults to text channel')
+                        .addChoices({ name: 'TEXT', value: 'Guild_Text' }, { name: 'VOICE', value: 'Guild_Voice' }))
+                .addChannelOption(option =>
+                    option.setName('category')
+                        .setDescription('The category under which the channel is to be created')
+                        .addChannelTypes(CT.GuildCategory))
+                .addStringOption(option =>
+                    option.setName('topic')
+                        .setDescription('The topic of the channel to be created'))),
     async execute(interaction) {
         await interaction.deferReply();
         const sub = interaction.options.getSubcommand();
@@ -70,7 +89,53 @@ module.exports = {
                 };
                 return await interaction.editReply({ embeds: [embed] });
             }
-
+            case 'create': {
+                const channel = await interaction.guild.channels.create(interaction.options.getString('name'), {
+                    parent: interaction.options.getChannel('category')?.id || null,
+                    type: interaction.options.getString('type') || 'Guild_Text',
+                    topic: interaction.options.getString('topic') || null,
+                });
+                return await interaction.editReply({ embeds: [{
+                    title: 'Channel Created',
+                    color: 15277667,
+                    fields: [
+                        {
+                            name: 'Name',
+                            value: `${channel.name} | <#${channel.id}>`,
+                            inline: true,
+                        },
+                        {
+                            name: 'ID',
+                            value: channel.id,
+                            inline: true,
+                        },
+                        {
+                            name: '\u200b',
+                            value: '\u200b',
+                            inline: true,
+                        },
+                        {
+                            name: 'Type',
+                            value: casify(channel.type),
+                            inline: true,
+                        },
+                        {
+                            name: 'Created at',
+                            value: `<t:${Math.floor(channel.createdAt.getTime() / 1000)}:F>`,
+                            inline: true,
+                        },
+                        {
+                            name: '\u200b',
+                            value: '\u200b',
+                            inline: true,
+                        },
+                        {
+                            name: 'Topic',
+                            value: channel.topic ? shorten(channel.topic, 1020) : 'No Topic Provided',
+                        },
+                    ],
+                }] });
+            }
             default:
                 return await interaction.editReply('Unknown subcommand found. Please report this issue to dev.');
         }
