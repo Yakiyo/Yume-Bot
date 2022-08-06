ae954e2a822c1649b42bf9ac512ca70fee91310b
Yakiyo <yakiyosama5855@gmail.com>
2022-08-06 13:16:54 +0000

    feat: create channel command
    - finish all sub command
    - add hasPerm from util
    - add builder data for clone, rename and delete
    - add code for clone, rename and delete

--- src/commands/moderation/channel.js
+++ src/commands/moderation/channel.js
@@ -1,15 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { ChannelType: CT } = require('discord-api-types/v9');
-const { casify, shorten } = require('../../util.js');
+const { casify, shorten, hasPerm } = require('../../util.js');
 
-/*
-    Channel commands
-        info
-        create
-        delete
-        rename
-        clone
-*/
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -39,8 +31,45 @@ module.exports = {
                         .addChannelTypes(CT.GuildCategory))
                 .addStringOption(option =>
                     option.setName('topic')
-                        .setDescription('The topic of the channel to be created'))),
+                        .setDescription('The topic of the channel to be created')))
+            .addSubcommand(sub =>
+                sub.setName('delete')
+                    .setDescription('Delete\'s a channel')
+                    .addChannelOption(option =>
+                        option.setName('channel')
+                            .setDescription('The channel to delete')
+                            .setRequired(true)
+                            .addChannelTypes(CT.GuildText, CT.GuildVoice, CT.GuildNews))
+                    .addStringOption(option =>
+                        option.setName('reason')
+                            .setDescription('Reason for deleting the channel')))
+            .addSubcommand(sub =>
+                sub.setName('rename')
+                    .setDescription('Renames a channel')
+                    .addChannelOption(option =>
+                        option.setName('channel')
+                            .setDescription('The channel to rename')
+                            .setRequired(true)
+                            .addChannelTypes(CT.GuildText, CT.GuildVoice, CT.GuildNews))
+                    .addStringOption(option =>
+                        option.setName('name')
+                            .setDescription('The new name of the channel')
+                            .setRequired(true)))
+            .addSubcommand(sub =>
+                sub.setName('clone')
+                    .setDescription('Clone\'s a channel')
+                    .addChannelOption(option =>
+                        option.setName('channel')
+                            .setDescription('The channel to clone')
+                            .setRequired(true)
+                            .addChannelTypes(CT.GuildText, CT.GuildVoice, CT.GuildNews))
+                    .addStringOption(option =>
+                        option.setName('name')
+                            .setDescription('The new name of the channel (optional)'))),
     async execute(interaction) {
+        if (interaction.options.getSubcommand() !== 'info') {
+            if (!hasPerm(interaction, 'MANAGE_ROLES')) return await interaction.editReply('You do not have the required permissions to use this subcommand.');
+        }
         await interaction.deferReply();
         const sub = interaction.options.getSubcommand();
 
@@ -135,6 +164,27 @@ module.exports = {
                         },
                     ],
                 }] });
+            }
+            case 'delete': {
+                const channel = interaction.options.getChannel('channel');
+                await interaction.guild.channels.delete(channel.id, interaction.options.getString('reason') || 'No Reason Provided');
+                return await interaction.editReply({ embeds: [{
+                    title: 'Channel deletion',
+                    color: 15277667,
+                    description: `Channel **${channel.name}** deleted\n**Moderator:** ${interaction.user.tag} | ${interaction.user.id}`,
+                }] });
+            }
+            case 'rename': {
+                const channel = interaction.options.getChannel('channel');
+                const newChannel = await channel.edit({ name: interaction.options.getString('name').replace(/ +/, '-').toLowerCase() });
+                return await interaction.editReply(`Successfully renamed **${channel.name}** to **${newChannel.name}**`);
+            }
+            case 'clone': {
+                const channel = interaction.options.getChannel('channel');
+                const cloned = await channel.clone({
+                    name: interaction.options.getString('name') || channel.name,
+                });
+                return await interaction.editReply(`Successfully cloned channel <#${cloned.id}> from <#${channel.id}>`);
             }
             default:
                 return await interaction.editReply('Unknown subcommand found. Please report this issue to dev.');
