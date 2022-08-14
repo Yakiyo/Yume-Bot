d35d739dccd223e872af1646c7f64a2691ad79fe
Yakiyo <yakiyosama5855@gmail.com>
2022-08-14 11:11:27 +0000

    feat: init db class
    - add properties to it

--- src/db/db.js
+++ src/db/db.js
@@ -1,1 +1,81 @@
-// Main file for db thingies
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+const { xp } = require('../config.json');
+
+/**
+ * Represents a user model
+ * @typedef {Object} User
+ * @property {string} id
+ * @property {number} xp
+ * @property {string} tagline
+ * @property {Date} updatedAt
+ */
+
+class Database {
+    constructor() {
+        this._connected = false;
+        this.connect();
+        this.prisma = prisma;
+    }
+    /**
+     * Connects to the database
+     * @private
+     * @returns void
+     */
+    async connect() {
+        if (this._connected) {
+            console.log('Connection already established');
+            return;
+        } else {
+            await prisma.$connect();
+            this._connected = true;
+            console.log('Successfully connected to the database');
+            return;
+        }
+    }
+    /**
+     * Create's a new User instance
+     * @param {string} id
+     * @returns User
+     */
+    async create(id) {
+        if (!id || typeof id !== 'string') throw new Error('Param id is either missing or is not a string');
+        return await prisma.user.create({
+            data: {
+                id: id,
+            },
+        });
+    }
+    /**
+     * Find's a User object
+     * @param {string} id
+     * @returns User
+     */
+    async find(id) {
+        return await prisma.user.findUnique({
+            where: id,
+        }) || null;
+    }
+    /**
+     * Increment's a user's xp
+     * @param {string} id
+     * @returns User
+     */
+    async increm(id) {
+        const profile = await this.find(id);
+        if (profile) {
+            return await prisma.user.update({
+                where: {
+                    id: profile.id,
+                },
+                data: {
+                    id: {
+                        increment: Math.random() * (xp.max - xp.min) + xp.min,
+                    },
+                },
+            });
+        }
+    }
+}
+
+module.exports = Database;
