671bea4fe5719f31bf3b0a86445ef667cdcc3fb1
Yakiyo <yakiyosama5855@gmail.com>
2022-08-26 12:25:10 +0000

    feat: update config
    - use alt bot id for development

---
+++ src/structs/commandHandler.js
@@ -0,0 +1,78 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Collection } = require('discord.js');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId } = require('../config.json');
+
+
+// This uses a secondary bot account to use for development instead of the original one.
+const botId = process.env.NODE_ENV !== 'development' ? clientId : '964798451261014026';
+
+/**
+ * Command Handler class
+ */
+class CommandHandler {
+    constructor() {
+        /**
+         * Collection of commands
+         * @type {Collection<string, Command>}
+         * @public
+         */
+        this.commands = new Collection();
+        this.init();
+    }
+    /**
+     * Initializes commands. Used only during class contruction
+     * @private
+     */
+    init() {
+        this.loadCommands();
+    }
+    /**
+     * Register's slash commands to discord
+     * @public
+     */
+    async registerCommands() {
+        const map = this.commands.size === 0 ? this.loadCommands() : this.commands;
+        const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+        try {
+            console.log('Started refreshing application (/) commands.');
+    
+            await rest.put(
+                Routes.applicationGuildCommands(botId, guildId),
+                { body: commands },
+            );
+    
+            console.log('Successfully reloaded application (/) commands.');
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    /**
+     * Load's all commands from directory
+     * @public
+     * @returns Collection<string, Command>
+     */
+    loadCommands() {
+        const commandFolders = fs.readdirSync('./src/commands');
+        for (const folder of commandFolders) {
+            const commandFiles = fs.readdirSync(`./src/commands/${folder}`).filter(file => file.endsWith('.js'));
+            for (const file of commandFiles) {
+                const command = require(path.resolve(process.cwd(), `./src/commands/${folder}/${file}`));
+                command.category = `${folder}`;
+                this.commands.set(command.data.name, command);
+            }
+        }
+        return this.commands;
+    }
+}
+
+module.exports = new CommandHandler();
+
+/**
+ * Represents a command object
+ * @typedef {Object} Command
+ * @property {Object} data
+ * @property {Function} execute
+ */
