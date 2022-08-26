91f7e7e6cb6eac196a7ed3b97da0a51fcf0e2ee0
Yakiyo <yakiyosama5855@gmail.com>
2022-08-26 12:43:40 +0000

    feat: add dev subcommand
    - add a subcom for registering slash commands

--- src/commands/system/dev.js
+++ src/commands/system/dev.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { owners } = require('../../config.json');
+const commandHandler = require('../../structs/commandHandler.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -11,7 +12,10 @@ module.exports = {
                 .addStringOption(option =>
                     option.setName('avatar')
                         .setDescription('Link the image to be used as avatar')
-                        .setRequired(true))),
+                        .setRequired(true)))
+        .addSubcommand(sub =>
+            sub.setName('load')
+                .setDescription('Registers slash commands')),
     async execute(interaction) {
         if (!owners.includes(interaction.user.id)) {
             return await interaction.reply({ ephemeral: true, content: 'Sorry. This command can only be used by bot developer' });
@@ -24,6 +28,11 @@ module.exports = {
                 } catch (error) {
                     return await interaction.reply('Unexpected error when changing avatar. Please provide a valid image link and make sure the image is not private');
                 }
+            }
+            case 'load': {
+                await interaction.deferReply();
+                await commandHandler.registerCommands();
+                return await interaction.editReply('Registered slash commands');
             }
             default:
                 return await interaction.reply({ content: 'Unknown subcommand.', ephemeral: true });
