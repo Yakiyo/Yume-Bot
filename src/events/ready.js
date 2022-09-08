db7bee5f596d3c6bd4787bafd5f3a616c2d7af31
Yakiyo <yakiyosama5855@gmail.com>
2022-09-08 19:28:21 +0000

    feat: send message on ready in prod else console log

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,13 +1,21 @@
 const { guildId } = require('../config.json');
+const { time } = require('@discordjs/builders');
+const { version } = require('../../package.json');
+const staffBot = '844253443510239262';
 
 module.exports = {
 	name: 'ready',
 	once: true,
 	async execute(client) {
-		console.log(`Ready and logged in as ${client.user.tag}`);
 		// Fetch guild members on ready
 		client.guilds.fetch(guildId).then(guild => {
 			guild.members.fetch();
 		});
+		if (process.env.NODE_ENV === 'production') {
+			client.channels.cache.get(staffBot)
+				.send(`${client.user.username} on!\nReady at ${time(client.readyAt)}\nGuilds: ${client.guilds.cache.size}, Channels: ${client.channels.cache.size}, Version: ${version}`);
+		} else {
+			console.log(`Ready and logged in as ${client.user.tag}`);
+		}
 	},
 };
